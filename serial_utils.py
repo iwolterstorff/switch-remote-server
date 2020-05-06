17e6f6c073e2f919578208c45b2e64afd12adfd5
Ian Wolterstorff <wolterstorff.i@husky.neu.edu>
2020-05-06 16:38:00 -0400

    Fix some serial issues

--- serial_utils.py
+++ serial_utils.py
@@ -20,8 +20,8 @@ class SerialReport(ctypes.Structure):
 
 def _dict_to_serial_report(input):
     report = SerialReport()
-    report.xp = ctypes.c_byte(input['lx'])
-    report.yp = ctypes.c_byte(input['ly'])
+    report.xp = ctypes.c_byte(int(input['lx']))
+    report.yp = ctypes.c_byte(int(input['ly']))
     report.rxp = ctypes.c_byte(0)
     report.ryp = ctypes.c_byte(0)
 
@@ -33,7 +33,7 @@ def _dict_to_serial_report(input):
     if input['y']:
         c |= SWITCH_Y
     report.c = ctypes.c_byte(c)
-    return report
+    return bytes(report)
 
 def set_serial_port(port):
     global _ser
